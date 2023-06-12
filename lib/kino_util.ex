cf92cf5282bf5f06291e82855bfd74cb55c94990
Marko Vukovic <mvkvc.git@gmail.com>
2023-06-12 00:35:49 -0400

    Save working prototype

--- lib/kino_util.ex
+++ lib/kino_util.ex
@@ -2,20 +2,54 @@ defmodule KinoUtil do
   use Kino.JS, assets_path: "lib/assets"
   use Kino.JS.Live
   use Kino.SmartCell, name: "System utilization"
-  # alias KinoUtil.Utils
+  alias KinoUtil.Utils
+
+  @update 3000
 
   @impl true
-  def handle_connect(ctx) do
-    {:ok, %{}, ctx}
+  def init(attrs, ctx) do
+    fields = %{
+      "cpu_util" => attrs["cpu_util"] || "",
+      "mem_used" => attrs["mem_used"] || "",
+      "mem_util" => attrs["mem_util"] || ""
+    }
+
+    Process.send(self(), "update", [])
+    {:ok, assign(ctx, fields: fields)}
   end
 
   @impl true
-  def to_attrs(_), do: %{}
+  def handle_info("update", ctx) do
+    cpu_util = Utils.cpu_util()
+    {mem_used, mem_util} = Utils.mem_util()
+
+    ctx =
+      update(
+        ctx,
+        :fields,
+        &Map.merge(&1, %{"cpu_util" => cpu_util, "mem_used" => mem_used, "mem_util" => mem_util})
+      )
+
+    broadcast_event(ctx, "update", [cpu_util, mem_used, mem_util])
+
+    Process.send_after(self(), "update", @update)
+    {:noreply, ctx}
+  end
 
   @impl true
-  def to_source(_) do
+  def handle_connect(ctx) do
+    {:ok, %{fields: ctx.assigns.fields}, ctx}
+  end
+
+  @impl true
+  def to_attrs(ctx) do
+    ctx.assigns.fields
+  end
+
+  @impl true
+  def to_source(_attrs) do
     quote do
-      IO.puts("Testing")
+      :ok
     end
     |> Kino.SmartCell.quoted_to_string()
   end
