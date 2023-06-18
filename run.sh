7a71470c25a8859a30ca02fa41260f68111f168f
Marko Vukovic <mvkvc.git@gmail.com>
2023-06-17 23:04:18 -0400

    Download and build in run script

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+pnpm install
+pnpm build
+
 # Issue with loading JS in Docker: Cross-Origin Request Blocked: The Same Origin Policy disallows reading the remote resource at http://0.0.0.0:8081/iframe/v4.html. (Reason: CORS request did not succeed). Status code: (null).
 # docker run \
 #     --rm \
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./lib/assets/*.js"],
+  theme: {
+    extend: {},
+  },
   plugins: [],
 };
