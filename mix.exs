9ae9a415be9c44eba100e72021b4c182b156f8fc
Marko Vukovic <mvkvc.git@gmail.com>
2023-06-23 22:51:29 -0400

    Hex package info

--- mix.exs
+++ mix.exs
@@ -1,26 +1,41 @@
 defmodule KinoUtil.MixProject do
   use Mix.Project
 
+  @name "kino_util"
+  @description "Livebook smart cell to show system utilization."
+  @version "0.1.0"
+
   def project do
     [
       app: :kino_util,
-      version: "0.1.0",
+      name: @name,
+      description: @description,
+      version: @version,
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
   def application do
     [
-      mod: {KinoUtil.Application, []},
-      extra_applications: [:logger, :os_mon]
+      extra_applications: [:logger, :os_mon],
+      mod: {KinoUtil.Application, []}
     ]
   end
 
   defp deps do
     [
       {:kino, "~> 0.9.4"}
+    ]
+  end
+
+  defp package() do
+    [
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE*),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/mvkvc/kino_util"}
     ]
   end
 end
